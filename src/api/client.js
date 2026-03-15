edfebad143f51d3741a72127296e32b2997bc676
yuyeh <49071263+yuyeh1212@users.noreply.github.com>
2026-03-15 17:50:37 +0800

    test1

--- src/api/client.js
+++ src/api/client.js
@@ -7,6 +7,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token =
     localStorage.getItem("access_token") || localStorage.getItem("bind_token");
+  console.log("🔑 request token:", token?.slice(0, 20), config.url);
   if (token) config.headers["Authorization"] = `Bearer ${token}`;
   return config;
 });
