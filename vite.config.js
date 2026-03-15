8b8df23c1f2c5c4b38499b70b5ac228243b0ff23
yuyeh <49071263+yuyeh1212@users.noreply.github.com>
2026-03-15 18:11:46 +0800

    強制更新

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/stock-dashboard/',
-})
+  base: "/stock-dashboard/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+      },
+    },
+  },
+});
